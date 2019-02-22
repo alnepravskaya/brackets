4f585fa9e480310202ff86c8391f0680b6da4429
Alena Kovalenko <alenakovalenko@Alenas-MacBook-Pro-2.local>
2019-02-22 14:44:19 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if(str.length%2!=0){
+        return false;
+    }
+    var arr=str.split('');
+    var stack=[];
+    for(var j=0;j<arr.length; j++){
+        for(var i=0; i<bracket.length;i++){
+            if(stack[stack.length-1]==bracket[i][0]&&arr[j]==bracket[i][1]){
+                stack.pop();
+            }else{
+                if(arr[j]==bracket[i][0]){
+                    stack.push(arr[j]);
+                }
+            }
+        }
+
+    }
+    return stack.length==0;
 }
